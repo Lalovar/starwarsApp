c2a50db27e8fad793d19b295736e1b15a5f6f9c5
lalovar <edvarluc@gmail.com>
2025-09-01 06:20:11 -0600

    Add scheduled command for stats computation in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,4 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->command('stats:compute')->cron('*/1 * * * *');
+        // Optional hardening:
+        // ->withoutOverlapping()->onOneServer();
+    })
+    ->create();
