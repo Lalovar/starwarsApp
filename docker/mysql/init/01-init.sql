79b828738f1f8e939a37e4cc1fc323440f1a6631
Eduardo Vargas <edvarluc@gmail.com>
2025-09-01 03:08:33 -0600

    refactor: simplify Docker setup

--- docker/mysql/init/01-init.sql
+++
@@ -1,6 +0,0 @@
--- Create database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS starwars_app CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Grant privileges to the application user
-GRANT ALL PRIVILEGES ON starwars_app.* TO 'starwars_user'@'%';
-FLUSH PRIVILEGES;
